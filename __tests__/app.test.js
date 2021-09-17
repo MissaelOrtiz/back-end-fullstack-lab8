79985deb4013783381761f47c41926ffb98e2b02
Missael Ortiz <ortiz.missael80@gmail.com>
2021-09-17 11:26:21 -0700

    added static get by id route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-// import Game from '../lib/models/Game';
+import Game from '../lib/models/Game';
 
 describe('game routes', () => {
   beforeEach(() => {
@@ -18,5 +18,13 @@ describe('game routes', () => {
     const res = await request(app).post('/api/v1/games').send(game);
 
     expect(res.body).toEqual({ id: '1', ...game });
+  });
+
+  it('gets a game by id via GET', async () => {
+    const info = { name: 'Dungeons and Dragons', medium: 'Tabletop Roleplay Game', genre: 'fantasy adventure' };
+    const game = await Game.insert(info);
+    const res = await request(app).get(`/api/v1/games/${game.id}`);
+
+    expect(res.body).toEqual(game);
   });
 });
