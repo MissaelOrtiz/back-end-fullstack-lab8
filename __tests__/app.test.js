71f3531fa084d428c46690160f64dcd2aedce937
Missael Ortiz <ortiz.missael80@gmail.com>
2021-09-17 11:12:58 -0700

    added app.js integration, added first games route test, not passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,21 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+// import Game from '../lib/models/Game';
 
-describe('demo routes', () => {
+describe('game routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('creates a game via POST', async () => {
+    const game = { name: 'Dungeons and Dragons', medium: 'Tabletop Roleplay Game', genre: 'fantasy adventure' };
+    const res = await request(app).post('/api/v1/games').send(game);
+
+    expect(res.body).toEqual({ id: '1', ...game });
   });
 });
